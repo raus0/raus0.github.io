45c4811190a99f244d20330dc43609e5c62050b7
raus0 <raurens000@gmail.com>
2023-09-27 22:50:38 +0900

    記事更新 Google Colabで均衡表の活用

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230927.2209';
+const cacheName = 'chirpy-20230927.2244';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
